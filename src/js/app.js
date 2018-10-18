0c0bea9bfd6851811a8fdc56afaf8e2b7f4b39ea
Janine Hempy <jhempy@gmail.com>
2018-10-18 10:21:11 -0400

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,32 @@
+// This isn't set up to copy yet!
+
+let app = new Vue({
+
+    el: "#app",
+
+    data: {
+
+        contacts: [
+            {
+                id: 1,
+                firstname: 'John',
+                lastname: 'Deer',
+                emailaddr: 'jdeer@example.com',
+                phonenum: '867-5309'
+            },
+            {
+                id: 2,
+                firstname: 'Jane',
+                lastname: 'Doe',
+                emailaddr: 'janed@example.com',
+                phonenum: '859-123-4567'
+            }
+        ]
+
+    },
+
+    methods: {
+
+    }
+
+});
