3ceafc1b0998f0b17224a703c6314453df7ab7c5
Janine Hempy <jhempy@gmail.com>
2018-10-18 11:23:08 -0400

    Populates form with contact data when user clicks edit

--- dist/js/app.js
+++ dist/js/app.js
@@ -61,6 +61,27 @@ let app = new Vue({
 
         },
 
+        editContact: function (contactId) {
+
+            // TODO: should we check that the contactID is valid?
+
+            let index = this.findIndexById(contactId);
+
+            this.formFirst = this.contacts[index].firstname;
+            this.formLast = this.contacts[index].lastname;
+            this.formEmail = this.contacts[index].emailaddr;
+            this.formPhone = this.contacts[index].phonenum;
+
+            this.showForm = true;
+
+        },
+
+        deleteContact: function (contactId) {
+
+            console.log("Time to delete contact # [" + contactId + "]");
+
+        },
+
         sortContacts: function (sortField) {
 
             console.log("Sorting contacts by [" + sortField + "]");
@@ -71,6 +92,16 @@ let app = new Vue({
 
             this.showForm = !this.showForm;
 
+        },
+
+        findIndexById: function (contactId) {
+            let index = -1;
+            for (let i=0; i < this.contacts.length; i++) {
+                if (contactId == this.contacts[i].id) {
+                    index = i;
+                }
+            }
+            return index;
         }
 
     }
